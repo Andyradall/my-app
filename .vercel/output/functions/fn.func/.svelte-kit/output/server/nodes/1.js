313358f1b609d304163b54c47b5e8c28f316445c
Andyradall <38905388+Andyradall@users.noreply.github.com>
2023-11-09 15:21:57 +0100

    reinstall vercel adapter

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++ .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.c6a2c717.js","_app/immutable/chunks/index.0a3f2fec.js","_app/immutable/chunks/singletons.f10f0307.js"];
+export const imports = ["_app/immutable/nodes/1.60046f59.js","_app/immutable/chunks/index.0a3f2fec.js","_app/immutable/chunks/singletons.17aa7eef.js"];
 export const stylesheets = [];
 export const fonts = [];
