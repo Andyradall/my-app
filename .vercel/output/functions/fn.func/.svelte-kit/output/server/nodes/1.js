93eee55eb45644abd953768275095ed18e5ab914
Andyradall <38905388+Andyradall@users.noreply.github.com>
2023-11-09 19:39:34 +0100

    test 2 about animejs

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++ .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.b823b9a3.js","_app/immutable/chunks/index.0a3f2fec.js","_app/immutable/chunks/singletons.4528f7cf.js"];
+export const imports = ["_app/immutable/nodes/1.5a744b70.js","_app/immutable/chunks/index.0a3f2fec.js","_app/immutable/chunks/singletons.bdc79c57.js"];
 export const stylesheets = [];
 export const fonts = [];
