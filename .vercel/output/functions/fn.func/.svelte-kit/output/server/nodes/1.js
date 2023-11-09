d971023dd9225b56ae3f7e71335fa7e27e51f906
Andyradall <38905388+Andyradall@users.noreply.github.com>
2023-11-09 15:16:59 +0100

    favicon.png

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++ .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.7d514d25.js","_app/immutable/chunks/index.0a3f2fec.js","_app/immutable/chunks/singletons.439db96f.js"];
+export const imports = ["_app/immutable/nodes/1.c6a2c717.js","_app/immutable/chunks/index.0a3f2fec.js","_app/immutable/chunks/singletons.f10f0307.js"];
 export const stylesheets = [];
 export const fonts = [];
