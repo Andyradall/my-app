cf0b4730d93653439d7f37bfdbb90cd0092b2dc8
Andyradall <38905388+Andyradall@users.noreply.github.com>
2023-11-21 22:44:49 +0100

    domain

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -78,7 +78,7 @@
      })
    </script>
 
-  <script>
+  <!--script>
     (function(h,o,t,j,a,r){
       h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
       h._hjSettings={hjid:3744201,hjsv:6};
@@ -87,5 +87,22 @@
       r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
       a.appendChild(r);
   })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-  </script>
+  </script-->
+  <script type="text/partytown">
+    (function (h, o, t, j, a, r) {
+      h.hj =
+        h.hj ||
+        function () {
+          (h.hj.q = h.hj.q || []).push(arguments);
+        };
+      h._hjSettings = { hjid: 3744201, hjsv: 6 };
+      a = o.getElementsByTagName('head')[0];
+      r = o.createElement('script');
+      r.async = 1;
+      r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+      a.appendChild(r);
+    })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+    </script>
 </svelte:head>
+
+
