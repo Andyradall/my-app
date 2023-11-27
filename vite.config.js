fdd6a1906dbd5da2500e656904e8b0e24ee2f819
Andyradall <38905388+Andyradall@users.noreply.github.com>
2023-11-27 12:13:05 +0100

    added build instructions for lightgallery to vite.config-js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { join } from 'path'
+import { join } from 'path';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { partytownVite } from '@builder.io/partytown/utils'
+import { partytownVite } from '@builder.io/partytown/utils';
 
 /** @type {import('vite').UserConfig} */
 const config = {
@@ -10,9 +10,14 @@ const config = {
       // `dest` specifies where files are copied to in production
       dest: join(process.cwd(), 'static', '~partytown')
     })
-  ]
+  ],
+  build: {
+    rollupOptions: {
+      external: ['lightgallery/plugins/MediumZoom']
+    }
+  }
 }
 
-
 export default config;
+
 
