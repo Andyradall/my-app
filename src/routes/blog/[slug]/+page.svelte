e858deba0af37e9aea209f913248dd60f8dfab2b
Andyradall <38905388+Andyradall@users.noreply.github.com>
2024-03-18 13:10:12 +0100

    working portabletext blog

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,18 +1,25 @@
 <script>
+  import { PortableText } from '@portabletext/svelte'; 
+  import WorkBack from '../../../components/WorkBack.svelte';
 	export let data;
   console.log("Text to identify this specific console.log:", data);
 </script>
 
-<article>
-  <header>
-    <h1>Sanity + SvelteKit</h1>
-  </header>
-  <main>
+<header class="relative top-0">
+	<WorkBack />
+</header>
+
+
+<article class="mx-auto font-euclid text-slate-600" >
+  <main class="md:max-w-6xl mx-auto justify-center px-4 pt-40 pb-20">
     <h2>Post starts</h2>
     {#if data.posts}
-      <h1 class="font-euclid font-bold text-lg">{data.posts.title}</h1>
+    <h1 class="text-[2.75rem] md:text-7xl font-euclid font-bold text-slate-600 py-4">{data.posts.title}</h1>
       <p>{data.posts.excerpt}</p>
-      <p class="font-euclid font-light">{data.posts.body}</p>
+    
+      <div class="font-euclid font-light">
+        <PortableText value={data.posts.body } components={{}} />
+      </div>
       <!-- Display other post details as needed -->
     {:else}
       <p>Post not found.</p>
